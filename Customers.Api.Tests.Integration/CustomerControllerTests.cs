914d68280d232c0b70e25accbb75cb993ec7c2ba
OMER KOSAR <omer.kosar@atmosware.turkcell.com.tr>
2025-01-22 09:13:02 +0300

    web application factory is added to run api in memory test method is added fro create end point. bogus is added to generate fake data

---
+++ Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Customers.Api.Tests.Integration
+{
+    public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+            .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+            .RuleFor(x => x.GithubUserName, "nickchapsas")
+            .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth)
+            .RuleFor(x => x.Email, faker => faker.Person.Email)
+            ;
+
+        public CustomerControllerTests(WebApplicationFactory<IApiMarker> webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
+        {
+
+            //Act
+            var response = await _httpClient.GetAsync($"api/customers/{Guid.NewGuid()}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+        [Fact]
+        public async Task Create_ReturnsCreated_WhenCustomerIsCreated()
+        {
+            var customer = _customerGenerator.Generate();
+
+            var response = await _httpClient.PostAsJsonAsync("api/customers/", customer);
+
+            var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+            customerResponse.Should().BeEquivalentTo(customer);
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+    }
+}
